bc75352198e06f8bc4ec01ab81836fdcc8632582
sabamajid <saba.majid@euphoria.pk>
2023-02-28 16:17:39 +0500

    deletePlacement

--- Assets/MapsData/Scripts/MapItem.cs
+++ Assets/MapsData/Scripts/MapItem.cs
@@ -1,27 +1,60 @@
-
-using System;
+using ARLocation.UI;
 using System.Collections;
-using Mapbox.Unity.Location;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using TMPro;
+using System.IO;
 
 public class MapItem : MonoBehaviour
 {
-//    public BountyLocations user;
-    //public TextMesh NameOnMap;
-    public void DisplayNames()
+    public static MapItem instance;
+    public Datum ARuser;
+    public GameObject DeletePlacementpanel;
+
+    private void Awake()
     {
-        //NameOnMap.text = user.id.ToString();
+        if (instance != null)
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
     }
-    /*void OnMouseDown()
+    private void OnMouseDown()
     {
-        Debug.Log(user.id);
-        LinkManager.instance.DisableLinksFields();
-        LinkUIManager.instance.EnableMapPanels(0);
-        LoadingManager.instance.loading.SetActive(true);
-        //LinkManager.instance.UsernameForLinksScreen.text = user.first_name.ToUpper() + " " + user.last_name.ToUpper();
-        //StartCoroutine(LinkManager.instance.GetAndDisplaySocialLinks(user.id));
-    }*/
-    
+        DeletePlacementpanel.SetActive(true);
+    }
+
+    public void DeletePlacement()
+    {
+        StartCoroutine(GetDeletePlacement());
+    }
+
+    IEnumerator GetDeletePlacement()
+    {
+        string requestName = "/api/v1/locations/destroy?loc_id=" + ARuser.id ;
+
+        using (UnityWebRequest www = UnityWebRequest.Get(AuthManager.BASE_URL + requestName))
+        {
+            www.SetRequestHeader("Authorization", "Bearer " + AuthManager.Token);
+            yield return www.SendWebRequest();
+            if (www.isNetworkError || www.isHttpError)
+            {
+                ConsoleManager.instance.ShowMessage("Network Error!");
+                SceneManager.LoadScene("Auth");
+            }
+            else
+            {
+                Debug.Log(www.downloadHandler.text);
+                SceneManager.LoadScene("DexiMap");
+            }
+        }
+    }
+
 }
+
